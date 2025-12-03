155c1e43675fdb02072c2d9a74c3830dfe8d64b4
Neha Gautam <nehahimanshirasna@gmail.com>
2025-12-04 01:03:41 +0530

    change server.js

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+    "https://rosewell-frontend.vercel.app"],
     credentials: true,
   })
 );
